5a5c1c9c26af661f9477a5a05ec34e0b980bff90
Andriy Kushnir (Orhideous) <me@orhideous.name>
2024-02-01 00:55:44 +0200

    Turn NiimbotPacket into dataclass

--- niimprint/packet.py
+++ niimprint/packet.py
@@ -1,30 +1,30 @@
+from dataclasses import dataclass
+
+
+@dataclass
 class NiimbotPacket:
-    def __init__(self, type_, data):
-        self.type = type_
-        self.data = data
+    kind: int
+    data: bytes
 
     @classmethod
     def from_bytes(cls, pkt):
         assert pkt[:2] == b"\x55\x55"
         assert pkt[-2:] == b"\xaa\xaa"
-        type_ = pkt[2]
-        len_ = pkt[3]
-        data = pkt[4 : 4 + len_]
+        kind = pkt[2]
+        length = pkt[3]
+        data = pkt[4 : 4 + length]
 
-        checksum = type_ ^ len_
+        checksum = kind ^ length
         for i in data:
             checksum ^= i
         assert checksum == pkt[-3]
 
-        return cls(type_, data)
+        return cls(kind, data)
 
     def to_bytes(self):
-        checksum = self.type ^ len(self.data)
+        checksum = self.kind ^ len(self.data)
         for i in self.data:
             checksum ^= i
         return bytes(
-            (0x55, 0x55, self.type, len(self.data), *self.data, checksum, 0xAA, 0xAA)
+            (0x55, 0x55, self.kind, len(self.data), *self.data, checksum, 0xAA, 0xAA)
         )
-
-    def __repr__(self):
-        return f"<NiimbotPacket type={self.type} data={self.data}>"
